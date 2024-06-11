9348c4a7376762c6c9dc1a9c66cb3a5582352012
Thabang Rammitlwa <thabang451@gmail.com>
2024-06-11 10:38:51 +0200

    App.jsx

--- blog-post/src/App.jsx
+++ blog-post/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { useState,useEffect } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+const [posts,setPosts] =useState([]);
+const [error , setError] =useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+useEffect(()=>{
+const fetchData = async()=>{
+  try{
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      if (!response.ok){
+        throw new Error (`Error fetching posts:${response.status}`);
+      }
+      const data = await response.json();
+      setPosts(data);
+  }catch (error) {
+  setError(error);
+  }
+}
+fetchData();
+},[])
+return (
+  <div className="App">
+    {error ? (
+      <p>Error: {error.message}</p>
+    ) : (
+      posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))
+    )}
+  </div>
+);
 }
 
-export default App
+export default App;
